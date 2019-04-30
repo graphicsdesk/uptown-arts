a0567e1701f70801a459462801e89812ac4a8da5
Jason Kao <jason.kao@columbia.edu>
2019-04-30 19:05:57 -0400

    image scrollytelling, a bit of font styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { Scrollama, Step } from 'react-scrollama';
 import injectSheet from 'react-jss';
+import copyString from './copy';
+import archieml from 'archieml';
+
+const { copy } = archieml.load(copyString);
 
 const styles = {
   main: {
@@ -16,7 +20,6 @@ const styles = {
     height: '100vh',
     top: 0,
     alignSelf: 'flex-start',
-    backgroundColor: '#aaa',
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
@@ -26,55 +29,120 @@ const styles = {
       color: '#fff',
     },
   },
+  img: {
+    position: 'absolute',
+    maxWidth: '95%',
+    maxHeight: '93%',
+    visibility: 'visible',
+    opacity: 1,
+    transitionDuration: '.3s',
+    boxShadow: '-1px 3px 10px -1px rgba(0,0,0,0.75)',
+  },
+  hideImg: {
+    position: 'absolute',
+    maxWidth: '95%',
+    maxHeight: '93%',
+    visibility: 'hidden',
+    opacity: 0,
+    transitionDuration: '.3s',
+    boxShadow: '-1px 3px 10px -1px rgba(0,0,0,0.75)',
+  },
   scroller: {
     flexBasis: '35%',
   },
   step: {
     margin: '0 auto 2rem auto',
-    paddingTop: 200,
-    paddingBottom: 200,
-    border: '1px solid #333',
-    '& p': {
-      textAlign: 'center',
-      padding: '1rem',
-      fontSize: '1.5rem',
-    },
-    '&:last-child': {
-      marginBottom: 0,
-    },
+    paddingTop: '200px',
+    paddingBottom: '200px',
+  },
+  artistName: {
+    fontFamily: 'Merriweather',
+    fontSize: '1.2rem',
+    fontWeight: 700,
+    margin: '0 0 1rem 0',
+  },
+  text: {
+    fontFamily: 'Merriweather',
+    fontSize: '1.2rem',
+    margin: '0 0 1rem 0',
+    lineHeight: '1.7',
+  },
+  credit: {
+    color: '#aaa',
+    fontSize: '1rem',
+    borderTop: '0.8px solid #ddd',
+    paddingTop: '0.8rem',
+    fontFamily: 'Open Sans',
   },
 };
 
 class App extends Component {
-  state = {
-    data: 0,
-    steps: [1, 2, 3],
+  constructor(props) {
+    super(props);
+    const { classes } = props;
+
+    this.images = [];
+
+    this.steps = copy.reduce((acc, artistObj) => {
+      const { artist, artworks } = artistObj;
+      artworks.forEach((artwork, artworkIdx) => {
+        const { credit, image, steps } = artwork;
+        this.images.push({
+          src: image,
+          alt: credit,
+        });
+
+        steps.forEach((step, stepIdx) => {
+          acc.push(
+            <Step data={image} key={step}>
+              <div className={classes.step}>
+                {artworkIdx === 0 && stepIdx === 0 && (
+                  <p className={classes.artistName}>{artist}</p>
+                )}
+                <p className={classes.text}>{step}</p>
+                {stepIdx === 0 && (
+                  <p className={classes.credit}>{credit}</p>
+                )}
+              </div>
+            </Step>
+          );
+        });
+      });
+      return acc;
+    }, []);
+
+    this.state = {
+      data: null,
+    };
   }
 
-  onStepEnter = ({ data }) => this.setState({ data });
+  onStepEnter = ({ data }) => {
+    console.log(data);
+    this.setState({ data });
+  }
 
   render() {
-    const { data, steps } = this.state;
+    const { data } = this.state;
     const { classes } = this.props;
     return (
       <div className={classes.main}>
         <div className={classes.graphic}>
-          <p>{data}</p>
+          {this.images.map(({ src, alt }) => (
+            <img
+              key={src}
+              className={data === src ? classes.img : classes.hideImg}
+              src={src}
+              alt={alt}
+            />
+          ))}
         </div>
         <div className={classes.scroller}>
           <Scrollama
             offset={0.33}
             onStepEnter={this.onStepEnter}
             onStepExit={this.onStepExit}
-            debug
           >
-            {steps.map((value, index) => (
-              <Step data={value} key={value + '-' + index}>
-                <div className={classes.step}>
-                  <p>step value: {value}</p>
-                </div>
-              </Step>
-            ))}
+            {this.steps}
           </Scrollama>
         </div>
       </div>
