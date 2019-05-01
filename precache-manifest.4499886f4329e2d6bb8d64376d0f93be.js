11d2d9c10c0b5ecdb92bffc1987bb1c20edb9a26
Jason Kao <jason.kao@columbia.edu>
2019-05-01 19:45:39 -0400

    Updates

---
+++ precache-manifest.4499886f4329e2d6bb8d64376d0f93be.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a9e1579ba4c2cd20301",
+    "url": "/spec-journalism/uptown-arts/static/js/main.7424c740.chunk.js"
+  },
+  {
+    "revision": "698cb26c8b2996800a5a",
+    "url": "/spec-journalism/uptown-arts/static/js/runtime~main.a5d6287e.js"
+  },
+  {
+    "revision": "e55a88b45074e6339789",
+    "url": "/spec-journalism/uptown-arts/static/js/2.24737297.chunk.js"
+  },
+  {
+    "revision": "5a5abcadf787f49722aee2a2b407a4fc",
+    "url": "/spec-journalism/uptown-arts/index.html"
+  }
+];
