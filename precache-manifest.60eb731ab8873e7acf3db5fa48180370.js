bfdcbcd1bcaace58c73f2b3aa619ff0fba4e8a8e
Jason Kao <jason.kao@columbia.edu>
2019-05-01 22:02:42 -0400

    Updates

---
+++ precache-manifest.60eb731ab8873e7acf3db5fa48180370.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ab976f15f56053d0de4",
+    "url": "/uptown-arts/static/js/main.50df5e07.chunk.js"
+  },
+  {
+    "revision": "2464772f0f62d2ceb2bb",
+    "url": "/uptown-arts/static/js/runtime~main.878daeb8.js"
+  },
+  {
+    "revision": "62535d5b0c7c42b1a23b",
+    "url": "/uptown-arts/static/js/2.51bd1326.chunk.js"
+  },
+  {
+    "revision": "b139ae83c7b30f4c422fe9e4ad913859",
+    "url": "/uptown-arts/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/uptown-arts/precache-manifest.49ace0b8544e5afd88b62aad65a7e085.js"
+  "/uptown-arts/precache-manifest.60eb731ab8873e7acf3db5fa48180370.js"
 );
 
 workbox.clientsClaim();
